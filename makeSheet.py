d70b29c51770322f6e8b60a9ed3301dea38d38f4
JoanChirinos <jchirinos@stuy.edu>
2018-06-17 12:48:34 -0400

    .

--- makeSheet.py
+++ makeSheet.py
@@ -41,7 +41,7 @@ def go():
               <body><form action="gradeSheet.py" method="post"><div class="container text-center">
 
               """, """
-                </div></form>
+                </div><input type="text" name="qnum" value='""" + str(qnum) + """' style="display: none;"></form>
                 <!-- Optional JavaScript -->
                 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
                 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
